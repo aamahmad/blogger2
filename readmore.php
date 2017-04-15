66efe1972dda36d5d3b85af61cc07092ef0d311f
moslembusana <moslembusana@gmail.com>
2017-04-16 02:58:08 +0700

    style readmore 90%

---
+++ readmore.php
@@ -0,0 +1,53 @@
+<div class="date-outer">
+ <div class="date-posts">    
+	<div class='post-outer'>
+		<div class='post hentry uncustomized-post-template' itemprop='blogPost' itemscope='itemscope' itemtype='http://schema.org/BlogPosting'>
+
+			
+
+			<div class='post-header'>
+			<div class='post-header-line-1'></div>
+			</div>
+
+			<div class='post-body entry-content' id='post-body-1632747976599459570' itemprop='description articleBody'>
+			<a href='http://insightzilla-facebook.blogspot.co.id/2017/04/adalah-toko-online-busana-muslim-pria.html'><img class='thumbnail-post' src='http://localhost/blogger2/img/expo-newsweek.jpg' title='adalah toko online busana muslim pria'/></a>
+
+			<div class="judul2">
+				<h3 class='post-title entry-title' itemprop='name'>
+				<a href='http://insightzilla-facebook.blogspot.co.id/2017/04/adalah-toko-online-busana-muslim-pria.html'>adalah toko online busana muslim pria</a>
+				</h3>
+			</div>
+
+			</div>
+
+			<div class='post-footer'>
+				<div class='post-footer-line post-footer-line-1'>
+				<span class='post-author vcard'>
+				</span>
+				<span class='post-timestamp'>
+				</span>
+				<span class='reaction-buttons'>
+				</span>
+				<span class='post-comment-link'>
+				</span>
+				<span class='post-backlinks post-comment-link'>
+				</span>
+				<span class='post-icons'>
+				</span>
+				<div class='post-share-buttons goog-inline-block'>
+				</div>
+				</div>
+				<div class='post-footer-line post-footer-line-2'>
+				<span class='post-labels'>
+				</span>
+				</div>
+				<div class='post-footer-line post-footer-line-3'>
+				<span class='post-location'>
+				</span>
+				</div>
+			</div>
+		</div>
+	</div>
+
+ </div>
+</div>
